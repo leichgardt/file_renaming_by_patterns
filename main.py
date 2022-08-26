c76d248fcb3a087b84043a93d6a1970c84ed44eb
leichgardt <TooEz3325948448_>
2022-08-26 18:02:18 +0300

    Added properties into PDF class. Code refactoring

--- main.py
+++ main.py
@@ -4,27 +4,52 @@ from typing import Callable
 
 
 class PDFFileObject:
-    def __init__(self, file: Path):
-        self._file = file
-        self.splitter = '_'
+    _path: Path
+    _splitter: str
 
-    def _get_filename_parts(self):
-        return self._file.stem.split(self.splitter)
+    def __init__(self, filepath: Path, splitter: str = '_'):
+        self.filepath = filepath
+        self.splitter = splitter
 
-    def rename(self, f_pattern: str, flt: Callable = None):
-        parts = self._get_filename_parts()
+    @property
+    def filepath(self) -> Path:
+        return self._path
+
+    @filepath.setter
+    def filepath(self, path: Path):
+        if not isinstance(path, Path):
+            raise ValueError('Given non-string splitter')
+        elif not path.is_file():
+            raise FileNotFoundError('File not found')
+        self._path = path
+
+    @property
+    def splitter(self) -> str:
+        return self._splitter
+
+    @splitter.setter
+    def splitter(self, val: str):
+        if not isinstance(val, str):
+            raise ValueError('Given non-string splitter')
+        self._splitter = val
+
+    def _get_splitted_filename(self):
+        return self._path.stem.split(self.splitter)
+
+    def rename_with_pattern(self, pattern: str, flt: Callable = None):
+        parts = self._get_splitted_filename()
         if flt is not None and not flt(parts):
             return False
-        path = self._file.resolve().parent
-        parts = f_pattern.format(*parts)
-        self._file.rename(f'{path}/{parts}.pdf')
+        path = self._path.resolve().parent
+        parts = pattern.format(*parts)
+        self._path.rename(f'{path}/{parts}')
         return True
 
-    def to_json(self):
-        return json.dumps({'filename': self._file.name, 'attrs': self._get_filename_parts()})
+    def get_json(self):
+        return json.dumps({'filename': self._path.name, 'attrs': self._get_splitted_filename()})
 
 
-def get_files(path: str, pattern):
+def get_files(path: str, pattern: str):
     return Path(path).glob(pattern)
 
 
@@ -37,10 +62,12 @@ def main():
     result = []
     for filepath in get_files(path='./files', pattern='*_*_*.pdf'):
         pdf = PDFFileObject(filepath)
-        data_before = pdf.to_json()
-        if not pdf.rename(f_pattern='{2}_{0}_{1}', flt=lambda x: 'j' not in x[-1].lower()):
+        data_before = pdf.get_json()
+        if not pdf.rename_with_pattern(
+                pattern='{2}_{0}_{1}.pdf',
+                flt=lambda x: 'j' not in x[-1].lower()):
             continue
-        data_after = pdf.to_json()
+        data_after = pdf.get_json()
         result.append({'before': data_before, 'after': data_after})
     save_to_json(result)
 
